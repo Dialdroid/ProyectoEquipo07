365e53b21eb83d623ddf6ce54944ac0f5b3696e6
alexcp99 <alexcebollapardo@gmail.com>
2018-11-08 13:51:47 +0100

    Botón Editar Perfil
    no lleva a nada

---
+++ Grupo7_App/app/src/main/java/grupo7/com/appg7/EditarPerfil.java
@@ -0,0 +1,15 @@
+package grupo7.com.appg7;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EditarPerfil extends Activity {
+
+    @Override
+    public void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.editar_mi_perfil);
+    }
+
+
+}

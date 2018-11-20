d7dd06fa42db782f4ad8ed54dc5c1e5ad3c75f9f
alexcp99 <alexcebollapardo@gmail.com>
2018-11-20 15:09:33 +0100

    Librerias
    Actualizando las librerias, debido a que estavan obsolteas

--- Grupo7_App/app/src/main/java/grupo7/com/appg7/MainActivity.java
+++ Grupo7_App/app/src/main/java/grupo7/com/appg7/MainActivity.java
@@ -35,38 +35,18 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.series.DataPoint;
-import com.jjoe64.graphview.series.LineGraphSeries;
 import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
         private Button AcercaDe;
 
-    //LineGraphSeries<DataPoint> series;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        /*double y,x;
-        x = -5.0;
-
-        GraphView graph = (GraphView) findViewById(R.id.graph);
-        series = new LineGraphSeries<DataPoint>();
-        for (int i = 0; i<500; i++ ){
-            x = x + 0.1;
-            y = Math.sin(x);
-            series.appendData(new DataPoint(x, y), true, 500);
-
-        }
-        graph.addSeries(series);
-        */
-
-
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);

4c8d4e0f28202f695ef984401088ecf80abe2a3f
alexcp99 <alexcebollapardo@gmail.com>
2019-01-12 13:40:25 +0100

    Editar Perfil
    Modificado el editar perfil con posibilidad de canviar la foto de perfil

--- Grupo7_App/app/src/main/java/grupo7/com/appg7/Fragment_Editar_Perfil.java
+++ Grupo7_App/app/src/main/java/grupo7/com/appg7/Fragment_Editar_Perfil.java
@@ -1,14 +1,17 @@
 package grupo7.com.appg7;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.DocumentsContract;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,6 +19,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.squareup.picasso.Picasso;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -25,6 +29,8 @@ import static com.facebook.FacebookSdk.getApplicationContext;
 
 public class Fragment_Editar_Perfil extends Fragment {
 
+    private static final int PICK_IMAGE = 100;
+    //Uri imageUri;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -47,6 +53,30 @@ public class Fragment_Editar_Perfil extends Fragment {
         final TextView cambiar_correo = (TextView) vista.findViewById(R.id.editEmail);
 
 
+        final ImageView photo = (ImageView) vista.findViewById(R.id.photoUser);
+
+
+        Uri foto_usuario = usuario.getPhotoUrl();
+
+
+        String fotoAuxiliar = "https://bit.ly/2zwrJ34";
+
+        Picasso.with(getActivity()).load(foto_usuario).fit().into(photo);
+
+
+        if (usuario.getPhotoUrl() == null) {
+            Picasso.with(getContext()).load(fotoAuxiliar).into(photo);
+        }
+
+        photo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+            }
+        });
+
+
+
 
 
         Button buttonOK = (Button)vista.findViewById(R.id.buttonOK);
@@ -61,7 +91,7 @@ public class Fragment_Editar_Perfil extends Fragment {
                 String nuevoNombre = cambiar_nombre.getText().toString();
                 String nuevoCorreo = cambiar_correo.getText().toString();
 
-                Toast toast1 = Toast.makeText(getApplicationContext(), "Toast por defecto", Toast.LENGTH_SHORT);
+                Toast toast1 = Toast.makeText(getApplicationContext(), "Datos modificados correctamente", Toast.LENGTH_SHORT);
                 // entra en el onclick
 
                 toast1.show();
@@ -90,6 +120,11 @@ public class Fragment_Editar_Perfil extends Fragment {
 
 
         return vista;
+    }
+
+    private void openGallery(){
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
     }
 
 

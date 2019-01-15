48045ef9c36806dd759bd20926057e39b007beba
Josep Carreres <jcarreresjr@hotmail.com>
2019-01-15 10:42:15 +0100

    Red de Sensores
    Cambio de subida de datos

--- Red de sensores/ClienteESP/ClienteESP.ino
+++ Red de sensores/ClienteESP/ClienteESP.ino
@@ -4,6 +4,7 @@
 #include "SensorMagnetico.h"
 #include "SensorMovimiento.h"
 #include "SensorGas2.h"
+#include "ConexionUDP.h"
 
 #include <TimeLib.h>
 #include <ArduinoJson.h>
@@ -33,7 +34,7 @@ SensorGas2 gas = SensorGas2(35);
 ConexionWiFi wifi = ConexionWiFi("Grupo7", "123456789");
 
 //Conexión UDP  // PUERTO
-//ConexionUDP udp = ConexionUDP(1234);
+ConexionUDP udp = ConexionUDP(1234);
 
 const char broker[] = "iot.eclipse.org";
 
@@ -64,27 +65,49 @@ void setup(){
     Serial.begin(115200);
     setTime (17, 45, 0, 22, 10, 2018); //hora minuto segundo dia mes año
     wifi.conectar();
-    client.begin(broker, net);
-    client.onMessage(messageReceived);
-    connect();   
+    udp.escuchar(wifi);
 }
 
 
 void loop(){
+   StaticJsonBuffer<200> jsonBuffer;                 //tamaño maximo de los datos
+   JsonObject& envio = jsonBuffer.createObject();    //creación del objeto "envio"
+
+    //Se crea el array de caracteres que posteriormente
+    //se rellenará con los datos a enviar
+    char envioTxt[200];
 
     //Se procesan todos los datos obtenidos de los sensores
     String fecha = String(day()) + "/" + String(month()) + "/" + String(year()) + "  " + String( hour()) + ":" + String(minute()) + ":" + String(second()); 
     String altura = String(distancia.calcularDistancia()) + "cm";
-    String movi = String(movimiento.detectarMovimiento());
-    String puerta= String(magn.puertaAbierta());
-    String haygas=String(gas.leerGas());
-    
-    
-    client.publish("grupo7/practica/gas",fecha);
+    String gas2= String(gas.leerGas());
 
+    //Se recopilan todos esos datos en un objeto JSON
+    envio["Hora"]=fecha;
+    envio["Altura"] = altura;
+    envio["Movimiento"] = movimiento.detectarMovimiento();
+    envio["Gas"]=gas2;
 
+    //Se copia el objeto JSON a un array de carácteres
+    envio.printTo(envioTxt);
 
-    
+    //Los datos se envían vía UDP en formato texto
+    udp.enviarDatos(envioTxt);
+
+    //Envío de datos a la Raspberry
+    if(Serial.available() > 0){
+    char command = (char)Serial.read();
+    switch(command){
+    case 'H':
+      Serial.println("Hola Mundo!");
+      //udp.enviarDatos("Hola Mundo!");
+      break;
+    case 'D':
+      Serial.print("DISTANCIA: ");
+      Serial.println(altura);
+      break;
+    }
+    }
     //Retardo de 1s entre medición y medición
     delay(1000);
 }

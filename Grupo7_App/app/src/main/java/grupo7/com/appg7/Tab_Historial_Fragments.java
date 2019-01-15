60f8cd6d8b577dfb806c3a145db31c27da026651
Josep Carreres <jcarreresjr@hotmail.com>
2019-01-15 16:59:37 +0100

    Historial Firestore

--- Grupo7_App/app/src/main/java/grupo7/com/appg7/Tab_Historial_Fragments.java
+++ Grupo7_App/app/src/main/java/grupo7/com/appg7/Tab_Historial_Fragments.java
@@ -22,12 +22,15 @@ import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.DataSet;
+import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -56,10 +59,13 @@ public class Tab_Historial_Fragments extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
-    private AlertDialog dialog;
-    FirebaseUser usuario = FirebaseAuth.getInstance().getCurrentUser();
+    ArrayList<Entry> values = new ArrayList<>();
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    ArrayList<HistorialVo> pesoArrayList;
+
     RecyclerView recyclerHistorial;
-    ArrayList<HistorialVo> listaHistorialDatosPeso;
 
 
     @Override
@@ -67,81 +73,20 @@ public class Tab_Historial_Fragments extends Fragment {
                              Bundle savedInstanceState) {
         View vista = inflater.inflate(R.layout.fragment_historial, container, false);
 
-        listaHistorialDatosPeso = new ArrayList<>();
+        pesoArrayList = new ArrayList<>();
         recyclerHistorial = (RecyclerView) vista.findViewById(R.id.recyclerId);
-        recyclerHistorial.setLayoutManager(new LinearLayoutManager(getContext())); //usamos getContext en vez de .this porque no es una actividad
+        recyclerHistorial.setLayoutManager(new LinearLayoutManager(getContext()));//usamos getContext en vez de .this porque no es una actividad
 
 
-        final AdapatadorHistorialPeso adapter = new AdapatadorHistorialPeso(listaHistorialDatosPeso);
-        recyclerHistorial.setAdapter(adapter);
 
-        Button buttonRemove = (Button) vista.findViewById(R.id.delete);
-        Button buttonAdd = (Button) vista.findViewById(R.id.add);
-        buttonRemove.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                android.support.v7.app.AlertDialog.Builder dialogo1 = new android.support.v7.app.AlertDialog.Builder(getContext());
-
-                dialogo1.setTitle("Advertencia");
-                dialogo1.setMessage("¿ Quiere borrar el ultimo peso?");
-                dialogo1.setCancelable(false);
-                dialogo1.setPositiveButton("Si", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialogo1, int id) {
-                        listaHistorialDatosPeso.remove(listaHistorialDatosPeso.size() - 1);
-                        adapter.notifyItemRemoved(listaHistorialDatosPeso.size());
-                    }
-                });
-                dialogo1.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialogo1, int id) {
-
-                    }
-                });
-                dialogo1.show();
-
-            }
-        });
-
-        /*
-        buttonAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                final AlertDialog.Builder mBuilder = new AlertDialog.Builder(getContext());
-                View mView = getLayoutInflater().inflate(R.layout.modal_layout, null);
-                final EditText newPeso = (EditText) mView.findViewById(R.id.edit_peso);
-                Button AddPeso = (Button) mView.findViewById(R.id.add_peso);
-                AddPeso.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-
-                        if (!newPeso.getText().toString().isEmpty()) {
-                            double numero = Integer.parseInt(newPeso.getText().toString());
-
-                            listaHistorialDatosPeso.add(listaHistorialDatosPeso.size(), new HistorialVo(numero, "Subtitulo nuevo titular", 1));
-                            adapter.notifyItemInserted(listaHistorialDatosPeso.size());
-                            Toast.makeText(getActivity(), "Peso añadido",
-                                    Toast.LENGTH_LONG).show();
-                            CloseDialog();
-
-                        } else {
-                            Toast.makeText(getActivity(), "Escribe tu nuevo peso",
-                                    Toast.LENGTH_LONG).show();
-                        }
-
-                    }
-                });
-
-                mBuilder.setView(mView);
-                dialog = mBuilder.create();
-                dialog.show();
-            }
-        });*/
+        loadDataFromFirestore();
+        loadLastDataFirestore();
 
         return vista;
 
 
     }
-/*
+
     private void loadDataFromFirestore() {
 
         if (pesoArrayList.size() > 0) {
@@ -159,20 +104,43 @@ public class Tab_Historial_Fragments extends Fragment {
 
                             Log.d(TAG, documentSnapshot.getId() + " => " + documentSnapshot.getData());
 
-                            HistorialVo mimedida = new HistorialVo(documentSnapshot.getDouble("peso"),documentSnapshot.getString("fecha"));
+                            HistorialVo mimedida = new HistorialVo(documentSnapshot.getDouble("Peso"),documentSnapshot.getString("Fecha"));
                             pesoArrayList.add(mimedida);
 
                         }
 
-                        adapter = new MyAdapter(TabPeso.this, pesoArrayList);
-                        mRecyclerView.setAdapter(adapter);
+
+
+                        AdapatadorHistorialPeso adaptador = new AdapatadorHistorialPeso(pesoArrayList);
+                        recyclerHistorial.setAdapter(adaptador);
 
                     }
                 });
 
-    }//*/
+    }//
 
-    private void CloseDialog() {
-        if (dialog != null) dialog.dismiss();
+    private void loadLastDataFirestore(){
+        final CollectionReference medidasInfo = db.collection("usuarios").document(user.getUid()).collection("Peso");
+
+        medidasInfo.orderBy("Fecha", Query.Direction.DESCENDING).limit(1)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        for (QueryDocumentSnapshot documentSnapshot : task.getResult()) {
+
+                            Log.d(TAG, documentSnapshot.getId() + " => " + documentSnapshot.getData());
+
+                            HistorialVo mimedida = new HistorialVo(documentSnapshot.getDouble("Peso"),documentSnapshot.getString("Fecha"));
+                            pesoArrayList.add(mimedida);
+
+
+
+                        }
+                    }
+                });
     }
+
 }
+
+

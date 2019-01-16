80096ec16009f0970cba5e4b2a22c68892753493
alexcp99 <alexcebollapardo@gmail.com>
2019-01-16 16:57:20 +0100

    Editar Altura
    Altura añadida a el información de usuario y el editar perfil

--- Grupo7_App/app/src/main/java/grupo7/com/appg7/MainActivity.java
+++ Grupo7_App/app/src/main/java/grupo7/com/appg7/MainActivity.java
@@ -11,10 +11,7 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.View;
@@ -27,9 +24,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -38,17 +33,11 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.squareup.picasso.Picasso;
-
-import java.util.Arrays;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -462,6 +451,11 @@ public class MainActivity extends AppCompatActivity
             String email_usuario = usuario.getEmail();
             Uri foto_usuario = usuario.getPhotoUrl();
             String numero_usuario = usuario.getPhoneNumber();
+
+            FirebaseFirestore db1 = FirebaseFirestore.getInstance();
+
+
+
 
             String fotoAuxiliar = "https://bit.ly/2zwrJ34";
 
